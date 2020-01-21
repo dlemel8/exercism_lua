e0177f0fd76dca4cd066f6cf3f29f76dc8a669fb
daniels <daniels@cybermdx.com>
2020-01-21 22:42:14 +0200

    solved hamming

--- hamming/hamming.lua
+++ hamming/hamming.lua
@@ -1,7 +1,18 @@
 local Hamming = {}
 
-function Hamming.compute(a,b)
+function Hamming.compute(a, b)
+    if #a ~= #b then
+        return -1
+    end
 
+    local res = 0
+    for i = 1, #b do
+        if a:sub(i,i) ~= b:sub(i,i) then
+            res = res + 1
+        end
+    end
+
+    return res
 end
 
 return Hamming
